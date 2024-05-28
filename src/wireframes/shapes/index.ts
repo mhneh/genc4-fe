b0ca0c5c56a77c4af9c0106741decd128a2ac5d3
mhneh <20120075@student.hcmus.edu.vn>
2024-05-28 20:30:41 +0700

    Remove redundant screen

--- src/wireframes/shapes/index.ts
+++ src/wireframes/shapes/index.ts
@@ -5,23 +5,23 @@
  * Copyright (c) Sebastian Stehle. All rights reserved.
  */
 
-import { RendererService } from "@app/wireframes/model/renderer/RendererService.ts";
-import { Register } from "@app/wireframes/shapes/components/register.ts";
-import { C4Control } from "@app/wireframes/model/renderer/impl/C4Control.ts";
-import { SignIn } from "@app/wireframes/shapes/components/signin.ts";
-import { Post } from "@app/wireframes/shapes/components/post.ts";
-import { ResetPassword } from "@app/wireframes/shapes/components/reset-password.ts";
-import { Security } from "@app/wireframes/shapes/components/security.ts";
-import { EmailComponent } from "@app/wireframes/shapes/components/email.ts";
-import { Database } from "@app/wireframes/shapes/containers/database.ts";
-import { SinglePageApplication } from "@app/wireframes/shapes/containers/spa.ts";
-import { ApiApplication } from "@app/wireframes/shapes/containers/api-application.ts";
-import { EmailSystem } from "@app/wireframes/shapes/contexts/email-system.ts";
-import { PersonalOwner } from "@app/wireframes/shapes/contexts/person.ts";
-import { RelationshipControl } from "@app/wireframes/model/renderer/impl/RelationshipControl.ts";
-import { Relationship } from "@app/wireframes/shapes/components/relationship.ts";
-import { AbstractControl } from "../model/renderer/impl/AbstractControl.ts";
-import { EmptySystem } from "./contexts/empty-system.ts";
+import {RendererService} from "@app/wireframes/model/renderer/RendererService.ts";
+import {Register} from "@app/wireframes/shapes/components/register.ts";
+import {C4Control} from "@app/wireframes/model/renderer/impl/C4Control.ts";
+import {SignIn} from "@app/wireframes/shapes/components/signin.ts";
+import {Post} from "@app/wireframes/shapes/components/post.ts";
+import {ResetPassword} from "@app/wireframes/shapes/components/reset-password.ts";
+import {Security} from "@app/wireframes/shapes/components/security.ts";
+import {EmailComponent} from "@app/wireframes/shapes/components/email.ts";
+import {Database} from "@app/wireframes/shapes/containers/database.ts";
+import {SinglePageApplication} from "@app/wireframes/shapes/containers/spa.ts";
+import {ApiApplication} from "@app/wireframes/shapes/containers/api-application.ts";
+import {EmailSystem} from "@app/wireframes/shapes/contexts/email-system.ts";
+import {PersonalOwner} from "@app/wireframes/shapes/contexts/person.ts";
+import {RelationshipControl} from "@app/wireframes/model/renderer/impl/RelationshipControl.ts";
+import {Relationship} from "@app/wireframes/shapes/components/relationship.ts";
+import {AbstractControl} from "../model/renderer/impl/AbstractControl.ts";
+import {EmptySystem} from "./contexts/empty-system.ts";
 import {
   Browser,
   Button,
@@ -54,51 +54,50 @@ import {
   VerticalLine,
   VerticalScrollbar,
 } from "./dependencies";
-import { ExternalSystem } from "@app/wireframes/shapes/contexts/external-system.ts";
-import { EmptyComponent } from "@app/wireframes/shapes/components/empty-component.ts";
-import { EmptyContainer } from "@app/wireframes/shapes/containers/empty-container.ts";
-import { CreateScreen } from "./screens/create.ts";
-import { ViewScreen } from "./screens/view.ts";
-import { UpdateScreen } from "./screens/update.ts";
-import { DeleteScreen } from "./screens/delete.ts";
-import { OtherScreen } from "./screens/other.ts";
+import {ExternalSystem} from "@app/wireframes/shapes/contexts/external-system.ts";
+import {EmptyComponent} from "@app/wireframes/shapes/components/empty-component.ts";
+import {EmptyContainer} from "@app/wireframes/shapes/containers/empty-container.ts";
+import {CreateScreen} from "./screens/create.ts";
+import {ViewScreen} from "./screens/view.ts";
+import {UpdateScreen} from "./screens/update.ts";
+import {OtherScreen} from "./screens/other.ts";
 
 export function registerShapeRenderers() {
-  RendererService.addRenderer(new AbstractControl(new Browser()));
-  RendererService.addRenderer(new AbstractControl(new Button()));
-  RendererService.addRenderer(new AbstractControl(new ButtonBar()));
-  RendererService.addRenderer(new AbstractControl(new Checkbox()));
-  RendererService.addRenderer(new AbstractControl(new ComboBox()));
-  RendererService.addRenderer(new AbstractControl(new Dropdown()));
-  RendererService.addRenderer(new AbstractControl(new Grid()));
-  RendererService.addRenderer(new AbstractControl(new Heading()));
-  RendererService.addRenderer(new AbstractControl(new HorizontalLine()));
-  RendererService.addRenderer(new AbstractControl(new HorizontalScrollbar()));
-  RendererService.addRenderer(new AbstractControl(new Icon()));
+  // RendererService.addRenderer(new AbstractControl(new Browser()));
+  // RendererService.addRenderer(new AbstractControl(new Button()));
+  // RendererService.addRenderer(new AbstractControl(new ButtonBar()));
+  // RendererService.addRenderer(new AbstractControl(new Checkbox()));
+  // RendererService.addRenderer(new AbstractControl(new ComboBox()));
+  // RendererService.addRenderer(new AbstractControl(new Dropdown()));
+  // RendererService.addRenderer(new AbstractControl(new Grid()));
+  // RendererService.addRenderer(new AbstractControl(new Heading()));
+  // RendererService.addRenderer(new AbstractControl(new HorizontalLine()));
+  // RendererService.addRenderer(new AbstractControl(new HorizontalScrollbar()));
+  // RendererService.addRenderer(new AbstractControl(new Icon()));
   // RendererService.addRenderer(new AbstractControl(new Image()));
-  RendererService.addRenderer(new AbstractControl(new Label()));
-  RendererService.addRenderer(new AbstractControl(new Link()));
-  RendererService.addRenderer(new AbstractControl(new List()));
-  RendererService.addRenderer(new AbstractControl(new Numeric()));
-  RendererService.addRenderer(new AbstractControl(new Paragraph()));
-  RendererService.addRenderer(new AbstractControl(new Phone()));
-  RendererService.addRenderer(new AbstractControl(new Progress()));
-  RendererService.addRenderer(new AbstractControl(new RadioButton()));
-  RendererService.addRenderer(new AbstractControl(new Raster()));
-  RendererService.addRenderer(new AbstractControl(new Rectangle()));
-  RendererService.addRenderer(new AbstractControl(new Shape()));
-  RendererService.addRenderer(new AbstractControl(new Slider()));
-  RendererService.addRenderer(new AbstractControl(new Tablet()));
-  RendererService.addRenderer(new AbstractControl(new Tabs()));
-  RendererService.addRenderer(new AbstractControl(new TextArea()));
-  RendererService.addRenderer(new AbstractControl(new TextInput()));
-  RendererService.addRenderer(new AbstractControl(new Toggle()));
-  RendererService.addRenderer(new AbstractControl(new VerticalLine()));
-  RendererService.addRenderer(new AbstractControl(new VerticalScrollbar()));
+  // RendererService.addRenderer(new AbstractControl(new Label()));
+  // RendererService.addRenderer(new AbstractControl(new Link()));
+  // RendererService.addRenderer(new AbstractControl(new List()));
+  // RendererService.addRenderer(new AbstractControl(new Numeric()));
+  // RendererService.addRenderer(new AbstractControl(new Paragraph()));
+  // RendererService.addRenderer(new AbstractControl(new Phone()));
+  // RendererService.addRenderer(new AbstractControl(new Progress()));
+  // RendererService.addRenderer(new AbstractControl(new RadioButton()));
+  // RendererService.addRenderer(new AbstractControl(new Raster()));
+  // RendererService.addRenderer(new AbstractControl(new Rectangle()));
+  // RendererService.addRenderer(new AbstractControl(new Shape()));
+  // RendererService.addRenderer(new AbstractControl(new Slider()));
+  // RendererService.addRenderer(new AbstractControl(new Tablet()));
+  // RendererService.addRenderer(new AbstractControl(new Tabs()));
+  // RendererService.addRenderer(new AbstractControl(new TextArea()));
+  // RendererService.addRenderer(new AbstractControl(new TextInput()));
+  // RendererService.addRenderer(new AbstractControl(new Toggle()));
+  // RendererService.addRenderer(new AbstractControl(new VerticalLine()));
+  // RendererService.addRenderer(new AbstractControl(new VerticalScrollbar()));
   // RendererService.addRenderer(new AbstractControl(new Window()));
   // RendererService.addRenderer(new AbstractControl(new Comment()));
-  RendererService.addRenderer(new AbstractControl(new Icon()));
-  RendererService.addRenderer(new AbstractControl(new Raster()));
+  // RendererService.addRenderer(new AbstractControl(new Icon()));
+  // RendererService.addRenderer(new AbstractControl(new Raster()));
 }
 
 export function registerComponents() {
@@ -127,7 +126,6 @@ export function registerComponents() {
   RendererService.addRenderer(new C4Control(new CreateScreen()));
   RendererService.addRenderer(new C4Control(new ViewScreen()));
   RendererService.addRenderer(new C4Control(new UpdateScreen()));
-  RendererService.addRenderer(new C4Control(new DeleteScreen()));
   RendererService.addRenderer(new C4Control(new OtherScreen()));
 
   RendererService.addRenderer(new RelationshipControl(new Relationship()));
